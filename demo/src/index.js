70c2dff63ae23b85856bcd9bf6efc872d5894da4
Brandon Fowler <brandfowl@gmail.com>
2023-12-03 21:06:14 -0500

    Improve demo code

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,25 @@
 import c from 'c-tiny';
 
-function emojiClicked(e) {
-    e.target.animate(
-        [
-            { transform: 'rotate(0) scale(1)' },
-            { transform: 'rotate(360deg) scale(0)' },
-        ],
+const EMOJIS = ['\u{1F386}', '\u{1F973}', '\u{1F51C}'];
+
+function createEmoji(emoji) {
+    return c(
+        'div',
         {
-            duration: e.target.duration,
-            iterations: 1
-        }
+            class: 'emoji',
+            $duration: Math.random() * 1500 + 500,
+            _click: e => e.target.animate(
+                [
+                    { transform: 'rotate(0) scale(1)' },
+                    { transform: 'rotate(360deg) scale(0)' },
+                ],
+                {
+                    duration: e.target.duration,
+                    iterations: 1
+                }
+            )
+        },
+        emoji
     )
 }
 
@@ -20,42 +30,13 @@ c(
     },
     c(
         'h1',
-        {
-            class: 'primary'
-        },
-        c('code', null, 'c-tiny'),
-        ' Demo!'
+        { class: 'primary' },
+        c('code', null, 'c-tiny'), ' Demo!'
     ),
     c(
         'div',
         { class: 'emoji-cnt' },
-        c(
-            'div',
-            {
-                class: 'emoji',
-                $duration: Math.random() * 1000 + 1000,
-                _click: emojiClicked
-            },
-            '\u{1F386}'
-        ),
-        c(
-            'div',
-            {
-                class: 'emoji',
-                $duration: Math.random() * 1000 + 1000,
-                _click: emojiClicked
-            },
-            '\u{1F973}'
-        ),
-        c(
-            'div',
-            {
-                class: 'emoji',
-                $duration: Math.random() * 1000 + 1000,
-                _click: emojiClicked
-            },
-            '\u{1F51C}'
-        )
+        ...EMOJIS.map(createEmoji)
     ),
     c('div', null, '(click above)')
 );
