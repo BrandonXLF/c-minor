d4a06387c2539d81b3c5087f08d5a8cc70c0fe3e
Brandon Fowler <42590338+BrandonXLF@users.noreply.github.com>
2024-09-23 14:19:24 -0400

    Add namespace support using "@" Fixes #2

--- src/c.js
+++ src/c.js
@@ -1,11 +1,16 @@
 export default function c(element, attrs, ...children) {
 	let isString = val => typeof val === 'string',
+		ns,
 		key,
 		val,
 		keyWithoutPrefix;
 
 	if (isString(element)) {
-		element = document.createElement(element);
+		[element, ns] = element.split('@');
+		
+		element = ns
+			? document.createElementNS(ns, element)
+			: document.createElement(element);
 	}
 
 	if (attrs instanceof Node || isString(attrs)) {
