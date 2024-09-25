b7c3d54844308538623d653f7f5d8dd0a1412a6e
Brandon Fowler <brandfowl@gmail.com>
2024-09-25 04:01:51 -0400

    Reduce size

--- src/c.js
+++ src/c.js
@@ -1,36 +1,32 @@
 export default function c(element, attrs, ...children) {
 	let isString = val => typeof val === 'string',
-		ns,
-		key,
+		keyOrNamespace,
 		val,
 		keyWithoutPrefix;
 
 	if (isString(element)) {
-		[element, ns] = element.split('@');
-		
-		element = ns
-			? document.createElementNS(ns, element)
-			: document.createElement(element);
+		[element, keyOrNamespace] = element.split('@');
+		element = document['createElement' + (keyOrNamespace ? 'NS' : '')](keyOrNamespace || element, element);
 	}
 
 	if (attrs instanceof Node || isString(attrs)) {
 		element.append(attrs);
 	} else if (attrs) {
-		for (key in attrs) {
-			val = attrs[key];
-			keyWithoutPrefix = key.slice(1);
+		for (keyOrNamespace in attrs) {
+			val = attrs[keyOrNamespace];
+			keyWithoutPrefix = keyOrNamespace.slice(1);
 			
-			if (key[0] == '_') {
+			if (keyOrNamespace[0] == '_') {
 				element.addEventListener(keyWithoutPrefix, val);
 				continue;
 			}
 
-			if (key[0] == '$') {
+			if (keyOrNamespace[0] == '$') {
 				element[keyWithoutPrefix] = val;
 				continue;
 			}
 
-			element.setAttribute(key, val);
+			element.setAttribute(keyOrNamespace, val);
 		}
 	}
 	
